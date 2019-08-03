9214c7a4fd4b7992d44b18900b213a9027c1a17a
Vitor Veras <vitorverasm@gmail.com>
2019-08-03 20:45:42 -0300

    test initial problem instance

---
+++ simplex/__main__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+from simplex.problems.canonical import Canonical
+
+if __name__ == '__main__':
+    # Definir um problema na forma normal
+    c = np.transpose(np.array([-10, -12, -12, 0, 0, 0]))
+    A = np.array([[1, 2, 2, 1, 0, 0], [2, 1, 2, 0, 1, 0], [2, 2, 1, 0, 0, 1]])
+    b = np.transpose(np.array([20, 20, 20]))
+
+    # Definir instancia do simplex
+    canonical_form_problem = Canonical(c, A, b)
+    canonical_form_problem.printRepresentation()
+
+    # Executar o algoritmo
+    # Exibir a saída
